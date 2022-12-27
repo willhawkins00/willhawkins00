e1f2bc7fbbe7b7ef2551cf56c44ce468295ecbea
willhawkins00 <90623150+willhawkins00@users.noreply.github.com>
2022-12-27 11:26:04 -0500

    Update OpenAIWebApp.py

--- OpenAIWebApp.py
+++ OpenAIWebApp.py
@@ -1,16 +1,15 @@
 import os
 import openai
 from flask import Flask
-
-os.environ['my_secret'] = "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"#API key saved as an environment variable 
-#print(os.getenv('my_secret'))
-openai.organization = "org-lw44KI9m0uVxbXSFSFkSuMvv"#OpenAI org ID
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv('my_secret')
 app = Flask(__name__)
 @app.route('/')
 
 def trial():
+    os.environ['my_secret'] = "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"#API key saved as an environment variable 
+    #print(os.getenv('my_secret'))
+    openai.organization = "org-lw44KI9m0uVxbXSFSFkSuMvv"#OpenAI org ID
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv('my_secret')
     input = input("Enter what you'd like the AI to do: ")
     response = openai.Completion.create(model="text-davinci-003", prompt=input, temperature=0, max_tokens=100)
     choices = response['choices']
